de35c05f29def826f9adc450d8d42fe884efbfa4
Rodrigo Assis Fialho <rodrigo.assis.fialho@gmail.com>
2017-11-03 18:36:18 -0200

    adding refresh feature

---
+++ frontend/tests/unit/routes/2players-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:2players', 'Unit | Route | 2players', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
